3d8353c34a2356e8ea22521e651ae6b1e03c9e73
InternMarketing <internmarketingcwi2@gmail.com>
2026-02-05 13:51:28 +0800

    Update slideshow.php

--- slideshow.php
+++ slideshow.php
@@ -1,27 +1,20 @@
-<?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
-  <title>SnapShow Stage</title>
-  <link rel="stylesheet" href="/assets/style.css">
+  <title>SnapShow</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
 
-  <div id="stage-wrapper">
-    <div class="side-beam left"></div>
-    <div class="side-beam right"></div>
-
-    <div id="stage">
-      <div id="spotlight"></div>
-      <div id="image-container"></div>
+  <div class="stage">
+    <div class="cinema">
+      <!-- slides injected here -->
     </div>
   </div>
 
-  <script src="/assets/app.js"></script>
+  <script src="assets/app.js"></script>
 </body>
 </html>
