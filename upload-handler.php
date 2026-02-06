2aec8da07400d3a45fcbb2939127e1449fd66576
InternMarketing <internmarketingcwi2@gmail.com>
2026-02-06 15:04:06 +0800

    Update upload-handler.php

--- upload-handler.php
+++ upload-handler.php
@@ -2,29 +2,23 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-/**
- * uploads folder at project root:
- * https://snapshow-swqb.onrender.com/uploads/
- */
 $dir = __DIR__ . '/uploads';
 
-// create uploads folder if missing
 if (!is_dir($dir)) {
-  mkdir($dir, 0777, true);
+    mkdir($dir, 0777, true);
 }
 
-// no files?
-if (!isset($_FILES['images'])) {
-  exit('No files received');
+if (empty($_FILES['images'])) {
+    die('No files received');
 }
 
 foreach ($_FILES['images']['tmp_name'] as $i => $tmp) {
-  if (!is_uploaded_file($tmp)) continue;
+    if (!is_uploaded_file($tmp)) continue;
 
-  $safeName = preg_replace('/[^a-zA-Z0-9._-]/', '_', $_FILES['images']['name'][$i]);
-  $name = time() . '_' . $safeName;
+    $safe = preg_replace('/[^a-zA-Z0-9._-]/', '_', $_FILES['images']['name'][$i]);
+    $name = time() . '_' . $safe;
 
-  move_uploaded_file($tmp, $dir . '/' . $name);
+    move_uploaded_file($tmp, $dir . '/' . $name);
 }
 
 echo 'OK';
