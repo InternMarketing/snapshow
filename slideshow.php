4f20a47bf83c4870e830995f2b7ed16f19244778
InternMarketing <internmarketingcwi2@gmail.com>
2026-02-06 17:15:53 +0800

    Update slideshow.php

--- slideshow.php
+++ slideshow.php
@@ -1,32 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>SnapShow</title>
-    <link rel="stylesheet" href="assets/style.css">
+<meta charset="UTF-8">
+<title>SnapShow</title>
+<link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
-    
-<div id="statusNote">⚠️ Photos are stored temporarily. Download backups during the event.</div>
 
 <div id="stage">
-
-    <!-- Side light beams -->
-    <div class="light-beam left"></div>
-    <div class="light-beam right"></div>
-
-    <!-- Curtains (THIS FIXES THE BLACK SIDES) -->
-    <div class="curtain left"></div>
-    <div class="curtain right"></div>
-
-    <!-- Spotlight -->
-    <div id="spotlight"></div>
-
-    <!-- Slideshow -->
-    <div id="slideshow"></div>
-
+  <div class="light-beam left"></div>
+  <div class="light-beam right"></div>
+  <div class="curtain left"></div>
+  <div class="curtain right"></div>
+  <div id="spotlight"></div>
+  <div id="slideshow"></div>
 </div>
 
-<script src="assets/app.js?v=2"></script>
+<script src="assets/app.js"></script>
 </body>
 </html>
