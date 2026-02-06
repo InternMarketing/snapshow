b27f49b9a0ebacd4d160d9b0ee9dfa2a8850c561
InternMarketing <internmarketingcwi2@gmail.com>
2026-02-06 14:58:08 +0800

    Update upload-handler.php

--- upload-handler.php
+++ upload-handler.php
@@ -1,8 +1,27 @@
 <?php
-$dir = __DIR__ . "/uploads/";
-if (!is_dir($dir)) mkdir($dir);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
 
-foreach ($_FILES["images"]["tmp_name"] as $i => $tmp) {
-  $name = time() . "_" . basename($_FILES["images"]["name"][$i]);
-  move_uploaded_file($tmp, $dir . $name);
+// uploads folder is at SITE ROOT
+$dir = realpath(__DIR__ . '/../uploads');
+if ($dir === false) {
+  $dir = __DIR__ . '/../uploads';
+  mkdir($dir, 0777, true);
 }
+
+if (!isset($_FILES['images'])) {
+  exit('No files received');
+}
+
+foreach ($_FILES['images']['tmp_name'] as $i => $tmp) {
+  if (!is_uploaded_file($tmp)) continue;
+
+  $name = time() . '_' . basename($_FILES['images']['name'][$i]);
+  $target = $dir . '/' . $name;
+
+  if (!move_uploaded_file($tmp, $target)) {
+    exit('Failed to move uploaded file');
+  }
+}
+
+echo 'OK';
