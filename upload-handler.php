3fde389d74575e7da4b50ef4d3108ab660a4cdb3
InternMarketing <internmarketingcwi2@gmail.com>
2026-02-06 16:18:03 +0800

    Update upload-handler.php

--- upload-handler.php
+++ upload-handler.php
@@ -2,41 +2,23 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-echo '<pre>';
-
-echo "FILES:\n";
-var_dump($_FILES);
-
 $dir = __DIR__ . '/uploads';
-echo "\nUpload dir: $dir\n";
 
 if (!is_dir($dir)) {
-    echo "Creating uploads dir...\n";
     mkdir($dir, 0777, true);
 }
 
-if (!is_writable($dir)) {
-    exit("Uploads directory NOT writable\n");
+if (!isset($_FILES['images'])) {
+    header('Location: upload.php?error=1');
+    exit;
 }
 
 foreach ($_FILES['images']['tmp_name'] as $i => $tmp) {
-    echo "\nProcessing file $i\n";
-
-    if (!is_uploaded_file($tmp)) {
-        echo "Not an uploaded file\n";
-        continue;
-    }
+    if (!is_uploaded_file($tmp)) continue;
 
     $name = time() . '_' . basename($_FILES['images']['name'][$i]);
-    $target = $dir . '/' . $name;
-
-    echo "Moving to $target\n";
-
-    if (move_uploaded_file($tmp, $target)) {
-        echo "SUCCESS\n";
-    } else {
-        echo "FAILED\n";
-    }
+    move_uploaded_file($tmp, $dir . '/' . $name);
 }
 
-echo "\nDone\n";
+header('Location: upload.php?success=1');
+exit;
