0afa64590b0067eece28adcc36b119e82d69a9e6
InternMarketing <internmarketingcwi2@gmail.com>
2026-02-06 15:33:21 +0800

    Create delete.php

---
+++ delete.php
@@ -0,0 +1,19 @@
+<?php
+$uploadDir = __DIR__ . '/uploads';
+
+if (!isset($_POST['files']) || !is_array($_POST['files'])) {
+    header('Location: /control.php');
+    exit;
+}
+
+foreach ($_POST['files'] as $file) {
+    $file = basename($file);
+    $path = $uploadDir . '/' . $file;
+
+    if (is_file($path)) {
+        unlink($path);
+    }
+}
+
+header('Location: /control.php');
+exit;
