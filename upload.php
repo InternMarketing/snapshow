b9d1d6d2962113d1307e31eb60b55c1c7e28157d
InternMarketing <internmarketingcwi2@gmail.com>
2026-02-23 14:57:22 +0800

    Update upload.php

--- upload.php
+++ upload.php
@@ -9,49 +9,45 @@
 </head>
 <body>
 
-    <main class="upload-container">
+<main class="upload-container">
 
-        <h1>Upload Your Photos</h1>
+    <h1>Upload Your Photos</h1>
 
-        <!-- ACTION BUTTONS -->
-        <div class="action-buttons">
-            <button id="showQR" type="button">
-                Show QR Code
-            </button>
+    <div class="action-buttons">
+        <button type="button" id="showQR">Show QR Code</button>
+        <a href="gallery.php" class="gallery-btn">View Gallery</a>
+    </div>
 
-            <a href="gallery.php" class="gallery-btn">
-                View Gallery
-            </a>
-        </div>
+    <div id="qrWrapper">
+        <div id="qrCode"></div>
+    </div>
 
-        <!-- QR CODE -->
-        <div id="qrWrapper">
-            <div id="qrCode"></div>
-        </div>
+    <form
+        id="uploadForm"
+        method="POST"
+        action="upload-handler.php"
+        enctype="multipart/form-data"
+    >
+        <input type="hidden" name="event_name" value="SnapShow">
 
-        <!-- UPLOAD FORM -->
-        <form id="uploadForm" enctype="multipart/form-data">
-            <input
-                type="file"
-                id="images"
-                name="images[]"
-                accept="image/*"
-                multiple
-                required
-            >
+        <input
+            type="file"
+            id="images"
+            name="images[]"
+            accept="image/*"
+            multiple
+            required
+        >
 
-            <button type="submit">
-                Upload Images
-            </button>
-        </form>
+        <button type="submit">Upload Images</button>
+    </form>
 
-        <p id="status"></p>
+    <p id="status"></p>
 
-    </main>
+</main>
 
-    <!-- Scripts -->
-    <script src="assets/qrcode.min.js"></script>
-    <script src="assets/upload.js"></script>
+<script src="assets/qrcode.min.js"></script>
+<script src="assets/upload.js"></script>
 
 </body>
 </html>
