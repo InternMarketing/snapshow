82f68429270c6114828c8565d7f31747cb59aeba
InternMarketing <internmarketingcwi2@gmail.com>
2026-02-23 15:46:00 +0800

    Update slideshow.php

--- slideshow.php
+++ slideshow.php
@@ -1,20 +1,14 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<title>SnapShow</title>
-<link rel="stylesheet" href="assets/style.css">
+    <meta charset="UTF-8">
+    <title>SnapShow Slideshow</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
 
-<div id="stage">
-  <div class="light-beam left"></div>
-  <div class="light-beam right"></div>
-  <div class="curtain left"></div>
-  <div class="curtain right"></div>
-  <div id="spotlight"></div>
-  <div id="slideshow"></div>
-</div>
+<div id="stage"></div>
 
 <script src="assets/app.js"></script>
 </body>
