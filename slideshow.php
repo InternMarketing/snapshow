4072242eae0ffe94c18970e9a49c5c85789fc6f1
InternMarketing <internmarketingcwi2@gmail.com>
2026-02-24 13:45:51 +0800

    Update slideshow.php

--- slideshow.php
+++ slideshow.php
@@ -1,31 +1,25 @@
+<?php
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <meta charset="UTF-8">
-<title>Slideshow</title>
+<title>SnapShow Slideshow</title>
 <link rel="stylesheet" href="assets/style.css">
 </head>
 <body>
 
-<div id="stage"></div>
-
-<div id="qrToggleBtn">QR</div>
-<div id="qrOverlay">
-    <div id="qrcode"></div>
+<div id="stage">
+    <img id="slideImage" src="" alt="">
 </div>
 
-<script src="assets/qrcode.min.js"></script>
-<script>
-new QRCode(
-    document.getElementById("qrcode"),
-    location.origin + "/upload.php"
-);
+<div id="controls">
+    <button id="toggleQR">Toggle QR</button>
+</div>
 
-qrToggleBtn.onclick = () => {
-    qrOverlay.style.display =
-        qrOverlay.style.display === "block" ? "none" : "block";
-};
-</script>
+<div id="qrWrapper" class="hidden">
+    <img src="qr.png" alt="Upload QR">
+</div>
 
 <script src="assets/app.js"></script>
 </body>
