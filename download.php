6bf0074f4321694d18013bf3c01c0601b5182f5a
InternMarketing <internmarketingcwi2@gmail.com>
2026-02-24 12:04:39 +0800

    Update download.php

--- download.php
+++ download.php
@@ -1,13 +1,35 @@
 <?php
-$zipPath = __DIR__ . '/latest.zip';
+session_start();
 
-if (!file_exists($zipPath)) {
-    header('Location: /zip.php');
+$data = json_decode(file_get_contents("php://input"), true);
+if (!is_array($data) || empty($data)) {
+    http_response_code(400);
     exit;
 }
 
-header('Content-Type: application/zip');
-header('Content-Disposition: attachment; filename="snapshow_latest.zip"');
-header('Content-Length: ' . filesize($zipPath));
-readfile($zipPath);
+$event = $_SESSION['event_name'] ?? 'event';
+$zipName = preg_replace('/[^a-zA-Z0-9_-]/', '', $event) . "_selected_images.zip";
+
+$tmp = tempnam(sys_get_temp_dir(), "snapshow_");
+$zip = new ZipArchive();
+$zip->open($tmp, ZipArchive::CREATE);
+
+$uploadDir = __DIR__ . "/uploads/";
+
+foreach ($data as $file) {
+    $file = basename($file);
+    $path = $uploadDir . $file;
+    if (is_file($path)) {
+        $zip->addFile($path, $file);
+    }
+}
+
+$zip->close();
+
+header("Content-Type: application/zip");
+header("Content-Disposition: attachment; filename=\"$zipName\"");
+header("Content-Length: " . filesize($tmp));
+
+readfile($tmp);
+unlink($tmp);
 exit;
